3e9bde0b7f7f3f0d7657a1ef786d59878740aebe
pknut <paulina.knut@gmail.com>
2017-02-04 09:04:25 +0100

    Computing GC Content

---
+++ GC.py
@@ -0,0 +1,12 @@
+import re
+
+def GC(filename='rosalind_gc.txt'):
+    with open(filename, 'r') as f:
+        data = f.read().replace('\n', '')
+
+    DNA = re.findall('[ATCG]+', data)
+    FASTA = re.findall('>Rosalind_\d+', data)
+    GC = [(i.count("G")+i.count("C"))/len(i)*100 for i in DNA]
+
+    print(FASTA[GC.index(max(GC))])
+    print(max(GC))
